3435348078f40222bbe43c421b2217676caa16fa
KenthaiColle <ken.k2009@hotmail.com>
2021-05-10 13:34:00 +1000

    First Commit (Code and Button)

---
+++ Puzzle/Assets/Script/OnOffButton.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffButton : MonoBehaviour
+{
+    public GameObject button;
+    private bool on = false;
+
+
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !on)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == button.name)
+                {
+                    Debug.Log("This is a Player");
+                    on = true;
+                    var cubeRenderer = button.GetComponent<Renderer>();
+                    cubeRenderer.material.SetColor("_Color", Color.green);
+                    Console.WriteLine("Done");
+                }
+                else
+                {
+                    Debug.Log("This isn't a Player");
+                }
+            }
+            
+        }
+        else if (Input.GetMouseButtonDown(0) && on)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+
+            }
+                if (hit.transform.name == button.name)
+                {
+                on = false;
+                var cubeRenderer = button.GetComponent<Renderer>();
+                cubeRenderer.material.SetColor("_Color", Color.red);
+            }
+            else
+            {
+                Debug.Log("This isn't a Player");
+            }
+
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(on == false)
+        {
+            var cubeRenderer = button.GetComponent<Renderer>();
+            cubeRenderer.material.SetColor("_Color", Color.red);
+        }
+        else if (on == true)
+        {
+            var cubeRenderer = button.GetComponent<Renderer>();
+            cubeRenderer.material.SetColor("_Color", Color.green);
+        }
+    }
+}
---
+++ Puzzle/Assets/Script/RotateWithMouse.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWithMouse : MonoBehaviour
+{
+     [SerializeField] float rotationSpeed = 100f;
+	 bool dragging = false;
+	 Rigidbody rb;
+ 
+     void Start()
+     {
+         rb = GetComponent<Rigidbody>();
+     }
+	 
+	 void OnMouseDrag()
+     {
+		dragging = true;
+		
+     }
+ 
+     void Update()
+     {
+		//Left mouse hold
+		if (Input.GetMouseButtonUp(0))
+			dragging = false;
+		
+     }
+	 void FixedUpdate(){
+		 if (dragging){
+			 float x = Input.GetAxis ("Mouse X") * rotationSpeed * Time.fixedDeltaTime;
+			 float y = Input.GetAxis ("Mouse Y") * rotationSpeed * Time.fixedDeltaTime;
+			 
+			//Rotate on Y axis
+			 rb.AddTorque (Vector3.down*x);
+			//Rotate on X axis
+			 rb.AddTorque (Vector3.right*y);
+		 }
+	 }
+			 
+}
